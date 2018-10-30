796eaeeb6b92978c739ec4080d5f47b7d896cf75
AlejandroBec <a.becor94@gmail.com>
2018-10-30 09:53:58 -0600

    Finish chat with server data

---
+++ client/main.js
@@ -0,0 +1,31 @@
+var socket = io.connect('http://172.18.2.156:6677', {'forceNew': true});
+socket.on('messages', function (data) {
+    console.log(data)
+    render(data)
+});
+
+function render(data) {
+    var html = data.map(function (message, index) {
+        return (`
+            <div class="message" >
+                
+                <strong>${message.nickname}</strong>
+                <p>${message.text}</p>
+                
+            </div>
+            `);
+    }).join(' ');
+    var div_msgs = document.getElementById("messages").innerHTML = html;
+    div_msgs.innerHTML = html;
+    div_msgs.screenTop = div_msgs.scrollHeight;
+}
+
+function addMessage(e) {
+    var message = {
+        nickname: document.getElementById('nickname').value,
+        text: document.getElementById('text').value
+    };
+    document.getElementById('nickname').style.display = 'none';
+    socket.emit('add-message', message);
+    return false;
+}
--- server/index.js
+++ server/index.js
@@ -3,10 +3,27 @@ var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
-app.get('/hola-mundo',function (req,res) {
+app.use(express.static('client'));
+
+app.get('/hola-mundo', function (req, res) {
     res.status(200).send("Hola Mundo")
 });
 
-server.listen(6677,function () {
+var messages = [{
+    id: 1,
+    text: 'Bienvenido al chat Bec',
+    nickname: 'Bec'
+}];
+
+io.on('connection', function (socket) {
+    console.log("El nodo con IP: " + socket.handshake.address + " se ha conectado...");
+    socket.emit('messages', messages);
+    socket.on('add-message', function (data) {
+        messages.push(data);
+        io.sockets.emit('messages', messages);
+    });
+});
+
+server.listen(6677, function () {
     console.log("Servidor en funcionamiento en http://localhost:6677/");
 });
